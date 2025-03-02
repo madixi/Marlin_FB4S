d00084b71ae3cf7a29ab120faa72b0543f882f09
ansible <ansible@local.home>
2025-03-02 08:36:11 +0000

    PID for backup hotcube

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -804,10 +804,10 @@ RN13S           - плата Robin Nano-s 1.3
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  13.38
-    #define DEFAULT_Ki   1.26
-    #define DEFAULT_Kd  35.56
-  #endif
+    #define DEFAULT_Kp 14.24
+    #define DEFAULT_Ki 1.17
+    #define DEFAULT_Kd 43.29
+#endif
 #endif
 
 /**
@@ -889,10 +889,10 @@ RN13S           - плата Robin Nano-s 1.3
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-	#define DEFAULT_bedKp 198.13
-	#define DEFAULT_bedKi 37.85
-	#define DEFAULT_bedKd 259.28
-  // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
+  #define DEFAULT_bedKp 61.41
+  #define DEFAULT_bedKi 11.63
+  #define DEFAULT_bedKd 216.18
+// FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
 
 //===========================================================================
